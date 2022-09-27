fd3ad10b0321afafd47c38a461de334e9cfee3ff
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-09-26 21:51:23 -0300

    lib configs | configurações da biblioteca

--- processa_envio.php
+++ processa_envio.php
@@ -1,4 +1,12 @@
-<?php 
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require './vendor/phpmailer/phpmailer/src/Exception.php';
+require './vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require './vendor/phpmailer/phpmailer/src/SMTP.php';
+
 
 // print_r($_POST);
 
@@ -31,4 +39,43 @@ $mensagem->__set('mensagem', $_POST['mensagem']);
 
 // print_r($mensagem);
 
-$mensagem->mensagemValida()? print'Mensagem válida' : print'A mensagem não é válida';
+if(!$mensagem->mensagemValida()) {
+    echo 'A mensagem não é válida';
+    die();
+}
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'gui.amarall123@gmail.com';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+    $mail->Port       = ;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('gui.amarall123@gmail.com', 'eu remetente');
+    $mail->addAddress('gui.amarall123@gmail.com', 'gui destinatario');     //Add a recipient
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Oi. Eu sou o assunto';
+    $mail->Body    = 'Oi. eu sou o conteudo do <b>e-mail</b>.';
+    $mail->AltBody = 'Oi. eu sou o conteudo do e-mail';
+
+    $mail->send();
+    echo 'Não foi possivel enviar este e-mail! Por favor tente novamente mais tarde';
+} catch (Exception $e) {
+    echo "Detalhes do erro: {$mail->ErrorInfo}";
+}
